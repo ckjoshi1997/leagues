b15be5f710e22ee02b1059d19ec0d19e627deb17
Chinmay <49972966+ckjoshi1997@users.noreply.github.com>
2020-07-19 14:53:58 -0700

    Updates

---
+++ precache-manifest.091e370677004e078bba6048aead4d2a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75c0ae86479ef91739406a6e26bf62b2",
+    "url": "/leagues/index.html"
+  },
+  {
+    "revision": "21609553a39f1b572904",
+    "url": "/leagues/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "412bd79a0e0c3b436e20",
+    "url": "/leagues/static/js/2.6d66d0c0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/leagues/static/js/2.6d66d0c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21609553a39f1b572904",
+    "url": "/leagues/static/js/main.d81bdcf5.chunk.js"
+  },
+  {
+    "revision": "211079ede9e6fd6c1958",
+    "url": "/leagues/static/js/runtime-main.7448a5ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/leagues/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "930399f0ca306dfebed75b8ad2f4db13",
+    "url": "/leagues/static/media/profile.930399f0.jpg"
+  }
+]);
